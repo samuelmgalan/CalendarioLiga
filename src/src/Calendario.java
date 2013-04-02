8732d35b0708d4c037787fb6b55fb53c0c45f1dc
Samuel <samuelmgalan@gmail.com>
2013-04-02 17:42:41 +0200

    Algoritmo terminado. Puede ser mas eficiente

--- src/src/Calendario.java
+++ src/src/Calendario.java
@@ -5,7 +5,7 @@ public class Calendario {
 	//Algoritmo: http://www.delphiaccess.com/forum/c-c-49/fixture-todos-contra-todos-en-c/
 	
 	private int[] equipos;
-	private String[][] matriz1,matriz2;
+	private String[][] matriz1,matriz2,jornadas;
 	
 	//Num de jornadas = (N-1)*2, con N = num equipos. (N-1) es una vuelta.
 	public Calendario(int N){
@@ -31,17 +31,50 @@ public class Calendario {
 			for(int j=0;j<N/2;j++){
 				matriz1[i][j] = String.valueOf(equipos[cont]);
 				cont++;
-				if(cont==5) cont=0;
-				System.out.print(matriz1[i][j]);
-			}
-		}
-		//matriz2
-		for(int i=0;i<N-1;i++){
-			for(int j=0;j<N/2;j++){
-				
+				if(cont==(N-1)) cont=0;
 			}
 		}
 		
+		//matriz2
+		cont = N-2;
+		for(int i=0;i<N-1;i++){
+			for(int j=0;j<N/2;j++){
+				if(j==0) matriz2[i][j] = String.valueOf(N);
+				else {
+					matriz2[i][j] = String.valueOf(equipos[cont]);
+					cont--;
+					if(cont==-1) cont = N-2;
+				}
+			}
+		}
+		
+		
+		//Elaboro la matriz final de enfrentamientos por jornada
+		/*  J1	1vs6	2vs5	3vs4
+			J2	4vs6	5vs3	1vs2
+			J3	2vs6	3vs1	4vs5
+			J4	5vs6	1vs4	2vs3
+			J5	3vs6	4vs2	5vs1
+		 */
+		jornadas = new String[N-1][N/2];
+		for(int i=0;i<N-1;i++){
+			for(int j=0;j<N/2;j++){
+				if(j==0){
+					if(i%2==0) jornadas[i][j] = matriz2[i][j] + "-" + matriz1[i][j] + " ";
+					else jornadas[i][j] = matriz1[i][j] + "-" + matriz2[i][j] + " ";
+				}
+				else jornadas[i][j] = matriz1[i][j] + "-" + matriz2[i][j] + " ";
+				System.out.print(jornadas[i][j]);
+				if(j==(N/2)-1) System.out.println();
+			}
+		}
+		/**
+		 *  6-1 2-5 3-4 
+			4-6 5-3 1-2 
+			6-2 3-1 4-5 
+			5-6 1-4 2-3 
+			6-3 4-2 5-1 
+		 */
 		
 	}
 	
@@ -49,7 +82,7 @@ public class Calendario {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new Calendario(6);
+		new Calendario(8);
 	}
 	
 }
