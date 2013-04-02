ee5b8ff804dde3fa580af5e6b91b29d16cc80c5e
Samuel <samuelmgalan@gmail.com>
2013-04-02 17:02:49 +0200

    Locales generados

--- src/src/Calendario.java
+++ src/src/Calendario.java
@@ -2,12 +2,12 @@ package src;
 
 public class Calendario {
 
-	//http://futbolymates.blogspot.com.es/2009/11/emparejamientos-y-calendario.html
+	//Algoritmo: http://www.delphiaccess.com/forum/c-c-49/fixture-todos-contra-todos-en-c/
 	
 	private int[] equipos;
-	private String[][] jornadas;
+	private String[][] matriz1,matriz2;
 	
-	//Num de jornadas = (N-1)*2, con N = num equipos
+	//Num de jornadas = (N-1)*2, con N = num equipos. (N-1) es una vuelta.
 	public Calendario(int N){
 		equipos = new int[N];
 		//Asigno posiciones del array a los equipos
@@ -15,34 +15,41 @@ public class Calendario {
 			equipos[i] = i+1;
 		}
 		
-		jornadas = new String[(N-1)*2][N/2];
-		//1ª jornada -> jornada[0]
-		int local = 0, visitante = N-1;
-		for (int j=0;j<N/2;j++){
-			jornadas[0][j] = equipos[local]+ "-" +equipos[visitante];
-			local++;
-			visitante--;
-			System.out.println(jornadas[0][j]);
+		matriz1 = new String[N-1][N/2];
+		matriz2 = new String[N-1][N/2];
+		//Relleno las matrices
+		/*   Matriz 1    	 Matriz 2			 
+			1   2   3		6   5   4
+			4   5   1		6   3   2
+			2   3   4		6   1   5
+			5   1   2		6   4   3
+			3   4   5		6   2   1
+		 */
+		int cont = 0;
+		//matriz1
+		for(int i=0;i<N-1;i++){
+			for(int j=0;j<N/2;j++){
+				matriz1[i][j] = String.valueOf(equipos[cont]);
+				cont++;
+				if(cont==5) cont=0;
+				System.out.print(matriz1[i][j]);
+			}
+		}
+		//matriz2
+		for(int i=0;i<N-1;i++){
+			for(int j=0;j<N/2;j++){
+				
+			}
 		}
 		
-		/*
-		 Las jornadas pares de la primera vuelta se forman a partir de la jornada anterior
-		   con estos cambios:
-			El equipo que está el último de la primera columna (equipo comodín) se coloca en
-		 el primer lugar de esa misma columna. El resto de los equipos de esa primera columna 
-		 bajan un lugar. A continuación se intercambian las dos columnas de las que consta
-		  la jornada.
-		 */
-		for(int i=2;i<N;i=i+2){
-			 
-		}
+		
 	}
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new Calendario(8);
+		new Calendario(6);
 	}
 	
 }
